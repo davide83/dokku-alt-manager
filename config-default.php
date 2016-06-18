e6fc1a59259a230e6d00ce70f8dc44ed61726258
Davide L <david@pluto-ws.lan>
2016-06-18 19:26:22 +0200

    fix jqueryu to version 1.11.4.min

--- config-default.php
+++ config-default.php
@@ -2,7 +2,7 @@
 $config['url_prefix']='?page=';
 $config['url_postfix']='';
 
-$config['js']['versions']['jqueryui']='1.11.3.min';
+$config['js']['versions']['jqueryui']='1.11.4.min';
 
 $config['dsn']='mysql://root:root@localhost/dam';
 
